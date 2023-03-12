d169501e2c73ddfe2b01761cb5b13b388a6ad684
VU3VWB <vu3vwb@gmail.com>
2023-03-12 14:14:17 +0800

    Add exception handling routine - will also remove the shmbuf upon exit

--- stdout2shmbuf/stdout2shmbuf.c
+++ stdout2shmbuf/stdout2shmbuf.c
@@ -5,10 +5,18 @@
 #include <sys/mman.h>
 #include <sys/stat.h>        /* For mode constants */
 #include <fcntl.h>           /* For O_* constants */
+#include <signal.h>
 
-#define SHM_FNAME "sh_rtl_1"
+#define SHM_FNAME "rbuff"
 #define XFER_SIZE 4096
 #define BUFFDEPTH 512
+
+static volatile int run_var = 1;
+
+void inter_handler(int dummy) 
+{
+    run_var = 0;
+}
 
 int main()
 {
@@ -21,13 +29,16 @@ int main()
     ftruncate(fd, BUFFDEPTH*XFER_SIZE);
     memaddr = (u_char*)mmap(0, BUFFDEPTH*XFER_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
 
-    printf("%p\n", memaddr);
-    while (1)
+    //printf("%p\n", memaddr);
+    signal(SIGINT, inter_handler);
+    while (run_var)
     { 
         read(STDIN_FILENO, memaddr+(buff_count*XFER_SIZE), XFER_SIZE);
         buff_count = (buff_count+1)%BUFFDEPTH;
     }
+    printf ("Signal caught, exiting the stdout2shmbuf code \n");
     munmap(memaddr, BUFFDEPTH*XFER_SIZE);
+    shm_unlink(SHM_FNAME);
     close(fd);
     return 0;
 }
