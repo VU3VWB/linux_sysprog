a7c2717495f3bbed5b1045d0c63d7f7b7533a855
VU3VWB <vu3vwb@gmail.com>
2023-05-24 15:54:13 +0800

    Fix a bug in converting ns to s

--- clocks/test_gettime.c
+++ clocks/test_gettime.c
@@ -8,9 +8,9 @@ struct timespec mytime;
 int res;
 double unixtime;
 res = clock_gettime(CLOCK_REALTIME, &mytime);
-printf ("UNIX timestamp is %ld\n", mytime.tv_sec);
-printf ("UNIX timestamp (ms) is %ld\n", mytime.tv_nsec);
-unixtime = (double)mytime.tv_sec + ((double)mytime.tv_nsec/1000);
+//printf ("UNIX timestamp is %ld\n", mytime.tv_sec);
+//printf ("UNIX timestamp (ms) is %ld\n", mytime.tv_nsec);
+unixtime = (double)mytime.tv_sec + ((double)mytime.tv_nsec/1e9);
 printf ("Full UNIX timestamp is %lf\n", unixtime);
 return 0;
 }
