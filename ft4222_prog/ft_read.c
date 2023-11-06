84750498a6ffbba69d49617777ad993ee656a9f8
VU3VWB <vu3vwb@gmail.com>
2023-11-07 06:58:56 +0800

    Add read codes

---
+++ ft4222_prog/ft_read.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include "ftd2xx.h"
+#include "libft4222.h"
+
+int main()
+{
+    FT_HANDLE ftHandle = NULL;
+    FT_STATUS ftStatus;
+    FT4222_STATUS ft4222Status;
+    ftStatus = FT_Open(0, &ftHandle);
+    if (FT_OK == ftStatus)
+    {
+        printf("Success ! \n");
+    }
+    ft4222Status = FT4222_SPIMaster_Init(ftHandle, SPI_IO_DUAL, CLK_DIV_128, CLK_IDLE_LOW, CLK_LEADING, 0x01);
+    if (FT4222_OK == ft4222Status)
+    {
+        printf("Success ! \n");
+    }
+
+    int n_bytes = 65536;
+    uint8 recvData[n_bytes];
+    uint8 dummy_buffer[10];
+    uint32 sizeTransferred;
+    // ft4222Status = FT4222_SPIMaster_SingleRead(ftHandle, &recvData[0], n_bytes, &sizeTransferred, true);
+    ft4222Status =  FT4222_SPIMaster_MultiReadWrite(ftHandle, &recvData[0], &dummy_buffer[0], 0, 0, n_bytes, &sizeTransferred);
+    if (FT4222_OK != ft4222Status)
+    {
+        printf ("Can't read \n");
+        printf ("%d\n",ft4222Status);
+        return 0;
+    }
+    printf ("Reached here \n");
+    // for (int i=0; i<n_bytes; i++)
+    // {
+    //     printf ("%d,", (int)recvData[i]);
+    // }
+
+    FILE *write_ptr;
+    write_ptr = fopen("spidata.bin","wb");
+    fwrite(recvData,sizeof(recvData),1,write_ptr);
+    fclose(write_ptr);
+
+    FT4222_UnInitialize(ftHandle);
+    FT_Close(ftHandle);
+    return 0;
+}
